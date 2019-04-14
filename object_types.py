2f7f665cc880ab14af6cbbab1bb3bd411a8f73ee
Scott Lowder <scott@scottlowder.net>
2019-04-14 20:10:06 +0000

    Change base class name

--- object_types.py
+++ object_types.py
@@ -1,6 +1,6 @@
 
 
-class TypeDefinition:
+class BaseManager:
     """
     Define allowed/expected fields for configuration object types.
     """
@@ -20,15 +20,18 @@ class TypeDefinition:
             if field not in self.config:
                 raise(Exception("Missing expected field '{}' for type '{}' in object '{}'. Expected fields are {}".format(field, self.__class__.__name__, self.config['_obj_name'], self.fields)))
 
-class Dummy(TypeDefinition):
+    def changes_required(self) -> bool:
+        """Determine whether applying this manager will cause a change."""
+
+class Dummy(BaseManager):
     pass
 
-class TestExample(TypeDefinition):
+class TestExample(BaseManager):
     fields = ['test1', 'test2']
 
-class ManagedFile(TypeDefinition):
+class ManagedFile(BaseManager):
     fields = ['path', 'name', 'permissions', 'owner', 'group', 'content']
 
-class ManagedPackage(TypeDefinition):
+class ManagedPackage(BaseManager):
     pass
 
