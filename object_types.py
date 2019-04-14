ed7d1c73b73e4ff580fc852268299853c27cada4
Scott Lowder <scott@scottlowder.net>
2019-04-14 19:31:30 +0000

    Better error messages. Stubs for new types.

--- object_types.py
+++ object_types.py
@@ -1,23 +1,34 @@
 
-VALID_TYPES = {
-    'file': ManagedFile,
-    'dummy': Dummy,
-    'package': ManagedPackage
-}
 
-Class TypeDefinition:
+class TypeDefinition:
     """
     Define allowed/expected fields for configuration object types.
     """
-    fields = ['_obj_name']
+    _special_fields = ['_obj_name', 'type']  # _obj_name and type are special fields that exist in all configuraiton objects.
+    fields = []
+
+    def __init__(self, obj):
+        self.config = obj
+        self.validate()
+
     def validate(self):
+        """Type-specific validation"""
         for key in self.config.keys():
-            if key not in self.fields and key != '_obj_name':
-                raise("Unexpected field '{}' in type '{}'. Expected fields are {}".format(key, self.__class__, self.fields))
-        for field in fields:
+            if key not in self.fields and key not in self._special_fields:
+                raise(Exception("Unexpected field '{}' for type '{}' in object '{}'. Expected fields are {}".format(key, self.__class__.__name__, self.config['_obj_name'], self.fields)))
+        for field in self.fields:
             if field not in self.config:
-                raise("Missing expected field '{}' for type '{}'. Expected fields are {}".format(field, self.__class__, self.fields))
+                raise(Exception("Missing expected field '{}' for type '{}' in object '{}'. Expected fields are {}".format(field, self.__class__.__name__, self.config['_obj_name'], self.fields)))
 
-
-Class ManagedFile(TypeDefinition):
+class Dummy(TypeDefinition):
     pass
+
+class TestExample(TypeDefinition):
+    fields = ['test1', 'test2']
+
+class ManagedFile(TypeDefinition):
+    fields = ['path', 'name', 'permissions', 'owner', 'group', 'content']
+
+class ManagedPackage(TypeDefinition):
+    pass
+
